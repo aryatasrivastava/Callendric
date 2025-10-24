472037b75a5037e032789ead849ab23dfe46615a
Aryata <srivastavaaryata2610@gmail.com>
2025-10-24 12:13:14 +0530

    feat(clerk): customise user menu

--- components/header.jsx
+++ components/header.jsx
@@ -1,32 +1,45 @@
 import React from "react";
-import Link from "next/link";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
-import { Button} from "./ui/button";
+import Link from "next/link";
+import { Button } from "./ui/button";
 import { PenBox } from "lucide-react";
+import UserMenu from "./ui/user-menu";
 
-const Header= () => {
-  return(
-<nav className="mx-auto py-2 px-4 flex justify-between items-center shadow-md border-b-2">
-   <Link href={"/"} className="flex items-center">
-   <Image
+const Header = ()=> {
+  return (
+    <nav className="mx-auto py-2 px-4 flex justify-between items-center shadow-md border-b-2">
+      <Link href="/" className="flex items-center">
+        <Image
           src="/logo.jpg"
-          width="200"
-          height="200"
+          width="150"
+          height="60"
           alt="Callendric Logo"
-          className="h-20 w-auto"
+          className="h-16 w-auto"
         />
-   </Link>
-    <div className="flex items-center gap-4">
+      </Link>
+
+      <div className="flex items-center gap-4">
         <Link href="/events?create=true">
           <Button variant="default" className="flex items-center gap-2">
-            <PenBox size={30} />
+            <PenBox size={18} />
             <span className="hidden sm:inline">Create Event</span>
           </Button>
         </Link>
-         <Button variant="outline">Login</Button>
-    </div>
-  </nav>
- );
-};
+        <SignedOut>
+          <SignInButton forceRedirectUrl="/dashboard">
+            <Button variant="outline">Login</Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserMenu />
+        </SignedIn>
+      </div>
+    </nav>
+      );
+
+}
+
+
 
 export default Header;
---
+++ components/ui/user-menu.jsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { UserButton } from "@clerk/nextjs";
+import { ChartNoAxesGantt } from "lucide-react";
+
+const UserMenu = () => {
+  return (
+    <UserButton
+      appearance={{
+        elements: {
+          avatarBox: "w-20 h-20",
+        },
+      }}
+    >
+      <UserButton.MenuItems>
+        <UserButton.Link
+          label="My Events"
+          labelIcon={<ChartNoAxesGantt size={20} />}
+          href="/events"
+        />
+        <UserButton.Action label="manageAccount" />
+      </UserButton.MenuItems>
+    </UserButton>
+  );
+};
+
+export default UserMenu;
